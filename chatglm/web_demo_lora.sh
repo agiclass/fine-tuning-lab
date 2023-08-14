ed0e144da10874f2202b365feae55b7a678cde0e
taliux <zrwang@gmail.com>
2023-08-14 18:49:43 +0800

    web demo lora

---
+++ chatglm/web_demo_lora.sh
@@ -0,0 +1,8 @@
+MODEL_PATH="/root/autodl-tmp/chatglm-6b"
+CHECKPOINT_DIR="PATH/TO/LORA/CHECKPOINT"
+
+CUDA_VISIBLE_DEVICES=0 python3 web_demo.py \
+    --model_name_or_path $MODEL_PATH \
+    --lora_checkpoint $CHECKPOINT_DIR \
+    --lora_rank 8 \
+    --lora_alpha 32
