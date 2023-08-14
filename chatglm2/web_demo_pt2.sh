89d85edb57da608de734307dc79bf726cbbf7d68
stark <wsd312@126.com>
2023-08-14 10:38:37 +0800

    update path

--- chatglm2/web_demo_pt2.sh
+++ chatglm2/web_demo_pt2.sh
@@ -1,7 +1,7 @@
 PRE_SEQ_LEN=128
 
-MODULE_PATH="PATH/TO/YOUR/MODULE"
-CHECKPOINT_DIR="PATH/TO/YOUR/CHECKPOINT"
+MODULE_PATH="/root/chatglm2-6b"
+CHECKPOINT_DIR="output/chatglm-6b-pt/20230806_154232/checkpoint-900/"
 
 CUDA_VISIBLE_DEVICES=0 python3 web_demo_pt2.py \
     --model_name_or_path $MODULE_PATH \
