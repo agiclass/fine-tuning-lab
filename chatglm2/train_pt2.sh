8f5a373536cb60a72af2aca48cb643e8e4f60958
taliux <zrwang@gmail.com>
2023-10-12 16:12:09 +0800

    pt2 batch_size=2

--- chatglm2/train_pt2.sh
+++ chatglm2/train_pt2.sh
@@ -19,14 +19,13 @@ LOCAL_RANK=-1 CUDA_VISIBLE_DEVICES=0 python3 main_pt2.py \
     --max_source_length 2048 \
     --max_target_length 1024 \
     --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 8 \
-    --gradient_accumulation_steps 16 \
+    --per_device_eval_batch_size 1 \
     --predict_with_generate \
     --evaluation_strategy steps \
-    --eval_steps 20 \
+    --eval_steps 320 \
     --num_train_epochs 1 \
-    --logging_steps 20 \
-    --save_steps 20 \
+    --logging_steps 320 \
+    --save_steps 320 \
     --learning_rate $LR \
     --pre_seq_len $PRE_SEQ_LEN \
     --quantization_bit 4
