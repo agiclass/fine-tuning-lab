87c89e980ef1908ace791ff766859d79b3b8e8f5
tong.liu <tju.liutong@gmail.com>
2023-10-13 09:53:41 +0000

    update gradio ui for chatglm2-6b lora finetune

--- web_demo/web_demo.py
+++ web_demo/web_demo.py
@@ -1,206 +1,116 @@
-import os, sys
-
-import gradio as gr
-import mdtex2html
-
-from transformers.generation.logits_process import LogitsProcessor
-from transformers.generation.utils import LogitsProcessorList
+#!/usr/bin/env python
+# coding=utf-8
+import sys
+sys.path.append('..')
+import os
+import re
+import json
 import torch
-import transformers
-from transformers import (
-    AutoConfig,
-    AutoModel,
-    AutoTokenizer,
-    AutoTokenizer,
-    DataCollatorForSeq2Seq,
-    HfArgumentParser,
-    Seq2SeqTrainingArguments,
-    set_seed,
-)
-from peft import get_peft_model, LoraConfig, TaskType
-from peft import PeftModel
-from arguments import ModelArguments, PeftArguments
+import numpy as np
+import gradio as gr
+import pandas as pd
+from transformers import AutoModel, AutoTokenizer, HfArgumentParser
+from peft import get_peft_model, LoraConfig, TaskType, PeftModel
 
-class InvalidScoreLogitsProcessor(LogitsProcessor):
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
-        if torch.isnan(scores).any() or torch.isinf(scores).any():
-            scores.zero_()
-            scores[..., 5] = 5e4
-        return scores
+from db_client import HotelDB
+from common.checkpoint_helper import load_lora_checkpoint
+from common.prompt_helper import build_prompt, build_response
+from common.arguments import ModelArguments, DataTrainingArguments, PeftArguments
 
-def build_prompt(context,question,device):
-    prompt = f"判例:\n{context}\n问题:\n{question}\n答案:\n"
-    inputs = tokenizer([prompt], return_tensors="pt")
-    inputs = inputs.to(device)
-    return inputs
-
-def generate(model, tokenizer, query: str, context: str, 
-                max_length: int = 8192, 
-                do_sample=False, 
-                top_p=0.8, 
-                temperature=0.8, 
-                logits_processor=None,
-                **kwargs
-    ):
-    
-    if logits_processor is None:
-        logits_processor = LogitsProcessorList()
-    logits_processor.append(InvalidScoreLogitsProcessor())
-    gen_kwargs = {"max_length": max_length, "do_sample": do_sample, "top_p": top_p,
-                    "temperature": temperature, "logits_processor": logits_processor, **kwargs}
-    
-    inputs = build_prompt(context,query,model.device)
-
-    gen_kwargs = {"max_length": max_length, "num_beams": 1, "do_sample": do_sample, "top_p": top_p,
-                      "temperature": temperature, "logits_processor": logits_processor, **kwargs}
-    outputs = model.generate(**inputs, **gen_kwargs)
-    outputs = outputs.tolist()[0][len(inputs["input_ids"][0]):]
-    response = tokenizer.decode(outputs)
-    response = model.process_response(response)
-    return response
-
-
-model = None
-tokenizer = None
-
-def parse_text(text):
-    """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
-    lines = text.split("\n")
-    lines = [line for line in lines if line != ""]
-    count = 0
-    for i, line in enumerate(lines):
-        if "```" in line:
-            count += 1
-            items = line.split('`')
-            if count % 2 == 1:
-                lines[i] = f'<pre><code class="language-{items[-1]}">'
-            else:
-                lines[i] = f'<br></code></pre>'
-        else:
-            if i > 0:
-                if count % 2 == 1:
-                    line = line.replace("`", "\`")
-                    line = line.replace("<", "&lt;")
-                    line = line.replace(">", "&gt;")
-                    line = line.replace(" ", "&nbsp;")
-                    line = line.replace("*", "&ast;")
-                    line = line.replace("_", "&lowbar;")
-                    line = line.replace("-", "&#45;")
-                    line = line.replace(".", "&#46;")
-                    line = line.replace("!", "&#33;")
-                    line = line.replace("(", "&#40;")
-                    line = line.replace(")", "&#41;")
-                    line = line.replace("$", "&#36;")
-                lines[i] = "<br>"+line
-    text = "".join(lines)
-    return text
-
-
-def predict(context, input, max_length, top_p, temperature):
-    
-    response = generate(
-            model, tokenizer, input, context, 
-            max_length=max_length, top_p=top_p,
-            temperature=temperature
+def init_model():
+    # 解析命令行参数
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, PeftArguments))
+    model_args, data_args, peft_args = parser.parse_args_into_dataclasses()
+    # 加载Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
+    # 加载Model
+    model = AutoModel.from_pretrained(model_args.model_name_or_path, trust_remote_code=True).half()
+    model.is_parallelizable = True
+    model.model_parallel = True
+    # 加载LoRA
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=False,
+        r=peft_args.lora_rank,
+        lora_alpha=peft_args.lora_alpha,
+        lora_dropout=peft_args.lora_dropout,
+        target_modules=["query_key_value"],
     )
+    raw_model = model
+    model = get_peft_model(model, peft_config).cuda()
+    if peft_args.lora_checkpoint is not None:
+        model = load_lora_checkpoint(raw_model, peft_args.lora_checkpoint).cuda()
+    return model, tokenizer, data_args.max_source_length, data_args.max_target_length
 
+def get_completion(prompt):
+    inputs = tokenizer(prompt, return_tensors="pt", truncation=True, padding=True, max_length=max_source_length)
+    inputs = inputs.to(model.device)
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_length=max_target_length + max_source_length + 1, num_beams=1, do_sample=False)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(response)
     return response
 
+# init gloab variables
+db = HotelDB()
+model, tokenizer, max_source_length, max_target_length = init_model()
 
-def reset_user_input():
-    return gr.update(value='')
-
+def chat(query, chatbot, context, search_field, return_field):
+    context.append({'role':'user','content':query})
+    response = get_completion(build_prompt(context))
+    # 判断以search命令结尾时去执行搜索
+    if re.search(r'\s*search:\s*\{.*?\}\s*$', response, re.DOTALL):
+        # 取出最新一条 'search:' 后面的json查询条件
+        match = re.findall(r'\s*search:\s*(\{.*?\})', response, re.DOTALL)[-1]
+        search_field = match.strip() if match else "{}"
+        try:
+            search_field = json.loads(search_field)
+            context.append({'role':'search','arguments':search_field})
+            return_field = db.search(search_field, limit=3)
+            context.append({'role':'return','records':return_field})
+            keys = return_field[0].keys() if return_field else []
+            data = {key: [item[key] for item in return_field] for key in keys}
+            data = data or {"hotel": []}
+            return_field = pd.DataFrame(data)
+        except Exception:
+            pass
+    # 如果不是search命令结尾，那么让LLM生成回复
+    response = get_completion(build_prompt(context))
+    match = re.findall(r'\s*assistant:(.*?)(。|\n|$)', response, re.DOTALL)[-1][0]
+    # reply = match[-1][0].strip() if match else "[LLM no reply] 我没有明白您的意思"
+    reply = match.strip() if match else "[LLM no reply] 我没有明白您的意思"
+    # 记录问答历史以供显示
+    chatbot.append((query, reply))
+    return chatbot, context, search_field, return_field
 
 def reset_state():
-    return "", "", ""
-
-
-with gr.Blocks() as demo:
-    gr.HTML("""<h1 align="center">ChatGLM2-6B</h1>""")
-    
-    context = gr.Textbox(show_label=False, placeholder="判决书...", lines=10).style(
-                    container=False)
-    
-    #chatbot = gr.Chatbot()
-    with gr.Row():
-        with gr.Column(scale=4):
-            with gr.Column(scale=12):
-                user_input = gr.Textbox(show_label=False, placeholder="问题...", lines=5).style(
-                    container=False)
-            with gr.Column(scale=12):
-                output = gr.Textbox(show_label=False, placeholder="答案...", lines=5).style(
-                    container=False)
-            with gr.Column(min_width=32, scale=1):
-                submitBtn = gr.Button("Submit", variant="primary")
-        with gr.Column(scale=1):
-            emptyBtn = gr.Button("Clear")
-            max_length = gr.Slider(0, 32768, value=8192, step=1.0, label="Maximum length", interactive=True)
-            top_p = gr.Slider(0, 1, value=0.8, step=0.01, label="Top P", interactive=True)
-            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
-
-    submitBtn.click(predict, [context, user_input, max_length, top_p, temperature],
-                    [output], show_progress=True)
-
-    emptyBtn.click(reset_state, outputs=[context, user_input, output], show_progress=True)
-
+    return [], []
 
 def main():
-    global model, tokenizer
+    with gr.Blocks() as demo:
+        gr.HTML("""<h1 align="center">Hotel Chatbot</h1>""")
 
-    parser = HfArgumentParser((ModelArguments, PeftArguments))
-    model_args, peft_args = parser.parse_args_into_dataclasses()
+        with gr.Row():
+            with gr.Column(scale=2):
+                chatbot = gr.Chatbot()
+                user_input = gr.Textbox(show_label=False, placeholder="输入框...", lines=2)
+                with gr.Row():
+                    submitBtn = gr.Button("提交", variant="primary")
+                    emptyBtn = gr.Button("清空")
+            with gr.Column(scale=2):
+                gr.HTML("""<h4>Search</h4>""")
+                search_field = gr.Textbox(show_label=False, placeholder="search...")
+                gr.HTML("""<h4>Return</h4>""")
+                return_field = gr.Dataframe()
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_args.model_name_or_path, trust_remote_code=True)
-    config = AutoConfig.from_pretrained(
-        model_args.model_name_or_path, trust_remote_code=True)
+        context = gr.State([])
 
-    if peft_args.pre_seq_len is not None:
-        config.pre_seq_len = peft_args.pre_seq_len
-        config.prefix_projection = peft_args.prefix_projection
+        submitBtn.click(chat, [user_input, chatbot, context, search_field, return_field],
+                        [chatbot, context, search_field, return_field])
+        emptyBtn.click(reset_state, outputs=[chatbot, context])
 
-    model = AutoModel.from_pretrained(model_args.model_name_or_path, config=config, trust_remote_code=True)
+    demo.queue().launch(share=False, server_name='0.0.0.0', inbrowser=True)
 
-    if peft_args.ptuning_checkpoint is not None:
-        print(f"Loading prefix_encoder weight from {peft_args.ptuning_checkpoint}")
-        prefix_state_dict = torch.load(os.path.join(peft_args.ptuning_checkpoint, "pytorch_model.bin"))
-        new_prefix_state_dict = {}
-        for k, v in prefix_state_dict.items():
-            if k.startswith("transformer.prefix_encoder."):
-                new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v
-        model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
-    elif peft_args.lora_checkpoint is not None:
-        peft_config = LoraConfig(
-            task_type=TaskType.CAUSAL_LM,
-            inference_mode=True,
-            r=peft_args.lora_rank,
-            lora_alpha=peft_args.lora_alpha,
-            lora_dropout=0,
-            target_modules=["query_key_value"],
-        )
-        model = get_peft_model(model, peft_config)
-        model.load_state_dict(torch.load(
-                os.path.join(peft_args.lora_checkpoint, "pytorch_model.bin")
-            ), 
-            strict=False
-        )
-        #model = PeftModel.from_pretrained(model,peft_args.lora_checkpoint)
-        #model = model.merge_and_unload()
-
-    if model_args.quantization_bit is not None:
-        print(f"Quantized to {model_args.quantization_bit} bit")
-        model = model.quantize(model_args.quantization_bit)
-    
-    model = model.cuda()
-    model = model.half()
-    
-    if peft_args.pre_seq_len is not None:
-        # P-tuning v2
-        model.transformer.prefix_encoder.float()
-    
-    model = model.eval()
-
-    demo.queue().launch(server_name="0.0.0.0", server_port=6006, share=False, inbrowser=True)
 
 if __name__ == "__main__":
     main()
--- web_demo/web_demo_lora.sh
+++ web_demo/web_demo_lora.sh
@@ -1,8 +1,13 @@
-MODEL_PATH="/root/chatglm2-6b"
+#!/bin/bash
+LORA_RANK=8
+MODEL_PATH="/root/autodl-tmp" # chatglm-6b所在目录
 CHECKPOINT_DIR="PATH/TO/LORA/CHECKPOINT"
 
 CUDA_VISIBLE_DEVICES=0 python3 web_demo.py \
-    --model_name_or_path $MODEL_PATH \
+    --overwrite_cache \
+    --model_name_or_path "${DATA_FS}/chatglm2-6b" \
     --lora_checkpoint $CHECKPOINT_DIR \
-    --lora_rank 8 \
-    --lora_alpha 32
+    --max_source_length 2048 \
+    --max_target_length 1024 \
+    --lora_rank $LORA_RANK \
+    --lora_alpha 32 
